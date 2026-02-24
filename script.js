5d21c1532c1bb731791f76134aaa9105c659ad73
asfsve <kohs42111@naver.com>
2026-02-24 19:19:14 +0900

    가운데

--- script.js
+++ script.js
@@ -149,4 +149,29 @@ todayBtn.addEventListener(".click" , () => {
 
 dateInput.addEventListener("keyup" , (e) => {
     dateInput.value = dateInput.value.replace(/[^0-9]/g, "");
+    if(dateInput.value.length === 2) {
+      dateInput.value += "/";
+    }
+    if(dateInput.value.length > 7) {
+      dateInput.value = dateInput.value.slice(0,7);
+    }
+   if(e.inputType = "deleteContentBackward") {
+    if(dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0 ,2);
+    }
+   }
 });
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] <13 && dateArr[1].length === 4) {
+      month == dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  alert("invalid date");
+}
